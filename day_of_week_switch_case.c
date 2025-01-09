e41f945715797b9a204853a904925af4f30f0470
Afrina Tithy <afrina.tithy@gmail.com>
2025-01-09 17:44:32 +0600

    day_of_week_switch_case.c

--- day_of_week_switch_case.c
+++ day_of_week_switch_case.c
@@ -1,1 +1,44 @@
 #include <stdio.h>
+int main()
+{
+    int week;
+
+    printf("Enter week numbers(1-7): ");
+    scanf("%d", &week);
+
+    switch (week)
+    {
+    case 1:
+        printf("Saturday");
+        break;
+
+    case 2:
+        printf("Sunday");
+        break;
+
+    case 3:
+        printf("Monday");
+        break;
+
+    case 4:
+        printf("Tuseday");
+        break;
+
+    case 5:
+        printf("Wednesday");
+        break;
+
+    case 6:
+        printf("Thursday");
+        break;
+
+    case 7:
+        printf("Friday");
+        break;
+
+    default:
+        printf("Invalid input");
+    }
+
+    return 0;
+}
