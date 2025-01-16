9819351462a70bc87b16df476b101fcb1148c19d
Afrina Tithy <afrina.tithy@gmail.com>
2025-01-16 22:52:08 +0600

    array.search

---
+++ array_search.js
@@ -0,0 +1,15 @@
+var i,n,arr1=[5,3,2,9,7];
+n=arr1.length;
+ var a = 9;
+ 
+
+ 
+ for(i=0; i<n; i++){
+     if(arr1[i]==a){
+         console.log("The item found at index "+i);
+         break;
+     }
+    
+ }
+ 
+     
