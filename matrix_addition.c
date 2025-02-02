409520f1f4256d4a2965f96b4a5d1f41ee5d9c07
Afrina Tithy <afrina.tithy@gmail.com>
2025-02-03 02:07:23 +0600

    matrix_addition.c

--- matrix_addition.c
+++ matrix_addition.c
@@ -1,8 +1,7 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-   int row ,column;
+   int row, column;
    
    printf("Enter row: ");
    scanf("%d", &row);
@@ -10,44 +9,35 @@ int main() {
    printf("Enter column: ");
    scanf("%d", &column);
    
-   int arr1[row][column],arr2[row][column], sum[row][column], i,j;
+   int arr1[row][column], arr2[row][column], sum[row][column], i, j;
  
-   
-   printf("Enter element of 1st metrix: \n");
-   for(i=0; i<row; i++){
-       for(j=0; j<column; j++){
+   printf("Enter elements of first matrix:\n");
+   for(i = 0; i < row; i++) {
+       for(j = 0; j < column; j++) {
            scanf("%d", &arr1[i][j]);
        }
    }
    
-    printf("Enter element of 2nd metrix: \n");
-   for(i=0; i<row; i++){
-       for(j=0; j<column; j++){
+   printf("Enter elements of second matrix:\n");
+   for(i = 0; i < row; i++) {
+       for(j = 0; j < column; j++) {
            scanf("%d", &arr2[i][j]);
        }
    }
+
+   for(i = 0; i < row; i++) {
+       for(j = 0; j < column; j++) {
+           sum[i][j] = arr1[i][j] + arr2[i][j];
+       }
+   }
   
-  
-  for(i=0; i<row; i++){
-      for(j=0; j<column; j++){
-          sum[i][j] = arr1[i][j] + arr2[i][j];
-         
-      }
-  }
-  
-   printf("sum of two metrix: \n");
-   for(i=0; i<row; i++){
-       for(j=0; j<column; j++){
-          printf("%d\t", sum[i][j]);
-         
+   printf("Sum of two matrices:\n");
+   for(i = 0; i < row; i++) {
+       for(j = 0; j < column; j++) {
+           printf("%d\t", sum[i][j]);
        }
        printf("\n");
    }
-  
-   
-   
-   
-   
 
-    return 0;
-} 
+   return 0;
+}
--- multiplicationOfTwoMatrix.c
+++ multiplicationOfTwoMatrix.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int m, p, n;
 
-   
-    printf("Enter the size of the matrix: ");
-    scanf("%d", &n);
+    
+    printf("Enter the number of rows and columns for the first matrix (m x p): ");
+    scanf("%d %d", &m, &p);
 
-    int first[n][n], second[n][n], result[n][n];
+    printf("Enter the number of rows and columns for the second matrix (p x n): ");
+    scanf("%d %d", &p, &n);
 
+    int first[m][p], second[p][n], result[m][n];
 
+  
     printf("Enter elements of the first matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
             scanf("%d", &first[i][j]);
         }
     }
 
    
     printf("Enter elements of the second matrix:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < p; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &second[i][j]);
         }
     }
 
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             result[i][j] = 0;
-            for (int k = 0; k < n; k++) {
+        }
+    }
+
+ 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < p; k++) {
                 result[i][j] += first[i][k] * second[k][j];
             }
         }
     }
 
- 
-    printf("multiplication Matrix:\n");
-    for (int i = 0; i < n; i++) {
+
+    printf("Multiplication Matrix:\n");
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             printf("%d ", result[i][j]);
         }
