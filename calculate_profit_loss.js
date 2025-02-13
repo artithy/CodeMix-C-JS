caf0ff42e22272fde1e1697624546890facc4771
Afrina Tithy <afrina.tithy@gmail.com>
2025-02-13 23:20:38 +0600

    calculate_profit_loss.js

---
+++ calculate_profit_loss.js
@@ -0,0 +1,14 @@
+const prompt= require("prompt-sync")();
+
+const costPrice = parseFloat(prompt("Enter Cost Price: "));
+const sellingPrice = parseFloat(prompt("Enter Selling Price: "));
+
+if (sellingPrice > costPrice) {
+    let profit = sellingPrice - costPrice;
+    console.log(`Profit: ${profit}`);
+} else if (sellingPrice < costPrice) {
+    let loss = costPrice - sellingPrice;
+    console.log(`Loss: ${loss}`);
+} else {
+    console.log("No Profit, No Loss");
+}
