a5d3b51732f1e6247df2dc2c11b7f7261f1264fc
Afrina Tithy <afrina.tithy@gmail.com>
2025-03-15 22:46:41 +0600

    sumPairIndices.js

--- sumPairIndices.js
+++ sumPairIndices.js
@@ -2,11 +2,11 @@ function findPairWithSum(numbers, target) {
     for (let i = 0; i < numbers.length; i++) {
         for (let j = i + 1; j < numbers.length; j++) {
             if (numbers[i] + numbers[j] === target) {
-                return [i, j]; 
+                return [i, j];
             }
         }
     }
-    return null; 
+    return null;
 }
 
 let numbers = [10, 20, 10, 40, 50, 60, 70];
